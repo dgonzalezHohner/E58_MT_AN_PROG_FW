c061d69a1560de85300113b197b242bce4331d4b
dgonzalezHohner <dgonzalez@encoderhohner.com>
2022-07-15 15:37:45 +0200

    Performing Answers for received commands

--- firmware/src/UART_COM.c
+++ firmware/src/UART_COM.c
@@ -255,14 +255,24 @@ void UART3Task(void)
         if(UART3RxCmdBuffer.CmdPtr[UART3RxCmdBuffer.RdIndex] != NULL)
         {
             CmdPtr = UART3RxCmdBuffer.CmdPtr[UART3RxCmdBuffer.RdIndex];
-            if(!strncmp(CmdPtr, "UserProg ON", strlen("UserProg ON")))              USR_SCL_EN_WR(1);
-            else if(!strncmp(CmdPtr, "UserProg OFF", strlen("UserProg OFF")))       USR_SCL_EN_WR(0);
-            else if(!strncmp(CmdPtr, "FractRange ON", strlen("FractRange ON")))     USR_SCL_FRACT_RNG_WR(1);
+            if(!strncmp(CmdPtr, "UserProg ON", strlen("UserProg ON")))
+            {
+                if(UARTTxCmdBufferAdd ("\2UserProg ON ACK\3")) USR_SCL_EN_WR(1);
+            }
+            else if(!strncmp(CmdPtr, "UserProg OFF", strlen("UserProg OFF")))
+            {
+                if(UARTTxCmdBufferAdd ("\2UserProg OFF ACK\3")) USR_SCL_EN_WR(0);
+            }
+            else if(!strncmp(CmdPtr, "FractRange ON", strlen("FractRange ON")))
+            {
+                if(UARTTxCmdBufferAdd ("\2FractRange ON ACK\3")) USR_SCL_FRACT_RNG_WR(1);
+            }
             else if(!strncmp(CmdPtr, "FractRange OFF", strlen("FractRange OFF")))   USR_SCL_FRACT_RNG_WR(0);
             else if(!strncmp(CmdPtr, "RollOver ON", strlen("RollOver ON")))         USR_SCL_ROLL_OVER_WR(1);
             else if(!strncmp(CmdPtr, "RollOver OFF", strlen("RollOver OFF")))       USR_SCL_ROLL_OVER_WR(0);
             else if(!strncmp(CmdPtr, "LimitSW ON", strlen("LimitSW ON")))           USR_SCL_LIMIT_SW_WR(1);
             else if(!strncmp(CmdPtr, "LimitSW OFF", strlen("LimitSW OFF")))         USR_SCL_LIMIT_SW_WR(0);
+            
             else if(!strncmp(CmdPtr, "FractRng = ", strlen("FractRng = ")))
             {
                 if(UART3CmdValParse(CmdPtr,strlen("FractRng = "),&TempVal))
@@ -270,80 +280,102 @@ void UART3Task(void)
                     if(((uint16_t)TempVal >= FRACT_RANGE_MIN)&&((uint16_t)TempVal <= FRACT_RANGE_MAX))
                     {
                         if(UARTTxCmdBufferAdd ("\2FractRng ACK\3")) CommonVars.FractRange = (uint16_t)TempVal;
-//                        SERCOM3_USART_Write("\2FractRng ACK\3",strlen("\2FractRng ACK\3"));
                     }
-//                    else SERCOM3_USART_Write("\2FractRng NAK\3",strlen("\2FractRng NAK\3"));
                     else UARTTxCmdBufferAdd ("\2FractRng NAK\3");
                 }
-//                else SERCOM3_USART_Write("\2FractRng Illegal\3",strlen("\2FractRng Illegal\3"));
                 else UARTTxCmdBufferAdd ("\2FractRng Illegal\3");
             }
+            
             else if(!strncmp(CmdPtr, "Factory MT Res = ", strlen("Factory MT Res = ")))
             {
                 if(UART3CmdValParse(CmdPtr,strlen("Factory MT Res = "),&TempVal))
                 {
-                    if((uint16_t)TempVal <= FACTORY_RESOMT_MAX) FACTORY_RESOMT_WR(TempVal);
-                    else{}//Value too high
+                    if((uint16_t)TempVal <= FACTORY_RESOMT_MAX)
+                    {
+                        if(UARTTxCmdBufferAdd ("\2Factory MT Res ACK\3")) FACTORY_RESOMT_WR(TempVal);
+                    }
+                    else UARTTxCmdBufferAdd ("\2Factory MT Res NAK\3");
                 }
-                else{}//invalid value
+                else UARTTxCmdBufferAdd ("\2Factory MT Res Illegal\3");
             }
+            
             else if(!strncmp(CmdPtr, "Factory OFFSET = ", strlen("Factory OFFSET = ")))
             {
                 if(UART3CmdValParse(CmdPtr,strlen("Factory OFFSET = "),&TempVal))
                 {
-                    if((uint16_t)TempVal <= FACT_OFFSET_MAX) CommonVars.FactOffset = (uint16_t)TempVal;
-                    else{}//Value too high
+                    if((uint16_t)TempVal <= FACT_OFFSET_MAX)
+                    {
+                        if(UARTTxCmdBufferAdd ("\2Factoru OFFSET ACK\3")) CommonVars.FactOffset = (uint16_t)TempVal;
+                    }
+                    else UARTTxCmdBufferAdd ("\2Factory OFFSET NAK\3");
                 }
-                else{}//invalid value
+                else UARTTxCmdBufferAdd ("\2Factory OFFSET Illegal\3");
             }
+            
             else if(!strncmp(CmdPtr, "IntDACLowLS = ", strlen("IntDACLowLS = ")))
             {
                 if(UART3CmdValParse(CmdPtr,strlen("IntDACLowLS = "),&TempVal))
                 {
                     if((uint16_t)TempVal <= INT_DAC_MAX)
                     {
-                        //Activar flag per traslladar el valor a la sortida
-                        CommonVars.IntDACLowLS = (uint16_t)TempVal;
+                        if(UARTTxCmdBufferAdd ("\2IntDACLowLS ACK\3"))
+                        {
+                            //Activar flag per traslladar el valor a la sortida
+                            CommonVars.IntDACLowLS = (uint16_t)TempVal;
+                        }
                     }
-                    else{}//Value too high
+                    else UARTTxCmdBufferAdd ("\2IntDACLowLS NAK\3");
                 }
-                else{}//invalid value
+                else UARTTxCmdBufferAdd ("\2IntDACLowLS Illegal\3");
             }
+            
             else if(!strncmp(CmdPtr, "IntDACLow = ", strlen("IntDACLow = ")))
             {
                 if(UART3CmdValParse(CmdPtr,strlen("IntDACLow = "),&TempVal))
                 {
                     if((uint16_t)TempVal <= INT_DAC_MAX)
                     {
-                        //Activar flag per traslladar el valor a la sortida
-                        CommonVars.IntDACLow = (uint16_t)TempVal;
+                        if(UARTTxCmdBufferAdd ("\2IntDACLow ACK\3"))
+                        {
+                            //Activar flag per traslladar el valor a la sortida
+                            CommonVars.IntDACLow = (uint16_t)TempVal;
+                        }
                     }
-                    else{}//Value too high
+                    else UARTTxCmdBufferAdd ("\2IntDACLow NAK\3");
                 }
-                else{}//invalid value
+                else UARTTxCmdBufferAdd ("\2IntDACLow Illegal\3");
             }
+            
             else if(!strncmp(CmdPtr, "ExtDACMAx = ", strlen("ExtDACMAx = ")))
             {
                 if(UART3CmdValParse(CmdPtr,strlen("ExtDACMAx = "),&TempVal))
                 {
-                    //Activar flag per traslladar el valor a la sortida
-                    CommonVars.ExtDACMax = (uint16_t)TempVal;
+                    if(UARTTxCmdBufferAdd ("\2ExtDACMax ACK\3"))
+                    {
+                        //Activar flag per traslladar el valor a la sortida
+                        CommonVars.ExtDACMax = (uint16_t)TempVal;
+                    }
                 }
-                else{}//invalid value
+                else UARTTxCmdBufferAdd ("\2ExtDACMax Illegal\3");
             }
+            
             else if(!strncmp(CmdPtr, "IntDACHighLS = ", strlen("IntDACHighLS = ")))
             {
                if(UART3CmdValParse(CmdPtr,strlen("IntDACHighLS = "),&TempVal))
                 {
                     if((uint16_t)TempVal <= INT_DAC_MAX)
                     {
-                        //Activar flag per traslladar el valor a la sortida
-                        CommonVars.IntDACHighLS = (uint16_t)TempVal;
+                        if(UARTTxCmdBufferAdd ("\2IntDACHighLS ACK\3"))
+                        {
+                            //Activar flag per traslladar el valor a la sortida
+                            CommonVars.IntDACHighLS = (uint16_t)TempVal;
+                        }
                     }
-                    else{}//Value too high
+                    else UARTTxCmdBufferAdd ("\2IntDACHighLS NAK\3");
                 }
-                else{}//invalid value
+               else UARTTxCmdBufferAdd ("\2IntDACHighLS Illegal\3");
             }
+            
             else if(!strncmp(CmdPtr, "SaveCfg", strlen("SaveCfg")))
             {
 
